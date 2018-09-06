5a35e21707f6bb7bd6738ee806f9eb5b52ec1ba7
sjhcome <sjhcome@gmail.com>
2018-09-06 21:32:47 +0900

    add XOR

--- deep_learning/foryou7242/chapter2/2_NAND_OR_XOR_AND.py
+++ deep_learning/foryou7242/chapter2/2_NAND_OR_XOR_AND.py
@@ -1,14 +1,62 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep  6 13:10:24 2018
+Spyder Editor
 
-@author: son
+This is a temporary script file.
 """
 
+def AND(x1, x2) :
+    x=np.array([x1, x2])
+    w= np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0;
+    else:
+        return 1;
+    
+    
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+def XOR(x1, x2):
+    x = np.array([x1, x2])
+    x1_ = NAND(x1, x2)
+    x2_ = OR(x1, x2)
+    return AND(x1_, x2_)
+    
+    
 
 
-
-import numpy as np
-
-print('hello')
+for i in range(0,2):
+    for j in range(0,2):
+        print("x {} y {} AND is {}" .format(i , j, AND(i,j)))
+for i in range(0,2):
+    for j in range(0,2):
+        print("x {} y {} NAND is {}" .format(i ,j, NAND(i,j)))
+for i in range(0,2):
+    for j in range(0,2):
+        print("x {} y {} OR is {}" .format (i ,j, OR(i,j)))
+for i in range(0,2):
+    for j in range(0,2):
+        print("x {} y {} XOR is {}" .format (i ,j, XOR(i,j)))
+        
+        
+        
